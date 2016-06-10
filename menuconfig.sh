c19b65297b62f597507641cd069ab34f36f148ad
James Christopher Adduono <jc@adduono.com>
2016-06-10 19:52:30 -0400

    Build scripts

---
+++ menuconfig.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# simple script for executing menuconfig
+
+# root directory of NetHunter kccat6 git repo (default is this script's location)
+RDIR=$(pwd)
+
+# directory containing cross-compile arm toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
+
+[ "$TARGET" ] || TARGET=stock
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=kccat6
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+DEFCONFIG_FILE=$RDIR/arch/$ARCH/configs/$DEFCONFIG
+
+[ -f "$RDIR/arch/$ARCH/configs/$DEFCONFIG" ] || {
+	echo "Config $DEFCONFIG not found in $ARCH configs!"
+	exit 1
+}
+
+cd "$RDIR"
+echo "Cleaning build..."
+rm -rf build
+mkdir build
+make -s -i -C "$RDIR" O=build "$DEFCONFIG" menuconfig
+#make -s -i -C "$RDIR" O=build apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_kccat6_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig menuconfig
+echo "Showing differences between old config and new config"
+echo "-----------------------------------------------------"
+if command -v colordiff >/dev/null 2>&1; then
+	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config | colordiff
+else
+	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config
+	echo "-----------------------------------------------------"
+	echo "Consider installing the colordiff package to make diffs easier to read"
+fi
+echo "-----------------------------------------------------"
+echo -n "Are you satisfied with these changes? Y/N: "
+read option
+case $option in
+y|Y)
+	cp build/.config "$DEFCONFIG_FILE"
+	echo "Copied new config to $DEFCONFIG_FILE"
+	;;
+*)
+	echo "That's unfortunate"
+	;;
+esac
+echo "Cleaning build..."
+rm -rf build
+echo "Done."
